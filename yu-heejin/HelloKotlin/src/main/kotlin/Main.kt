ab98b232d9810e31d9a85c7d185f16d9c045941d
yu-heejin <jkgt0521@naver.com>
2023-01-13 00:52:03 +0900

    [1주차] 1-2 개발환경 설정

---
+++ yu-heejin/HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // main function : 프로그램의 진입점
+    // main : no args
+    // maina : contain args
+    // Tools -> Kotlin -> Configure Kotlin Plugin Update를 누르면 플러그인을 최신으로 업데이트 할 수 있다.
+
+    println("Hello Kotlin")
+}

8c92b97b21611cd54ecb9904ecb537d92ab7a7b0
HannaSeo <shn2281@naver.com>
2023-01-26 22:26:46 +0900

    [3주차] 다양한 함수의 출격2! (익명함수, 확장함수)

---
+++ kittybelle/kotlin-part1/src/chap04/section3/ExtensionFunction.kt
@@ -0,0 +1,13 @@
+package chap04.section3
+
+fun main() {
+    val source = "Hello World!"
+    val target = "Kotlin"
+    println(source.getLongString(target))
+}
+
+// String을 확장해 getLongString 추가
+fun String.getLongString(target: String): String =
+        if (this.length > target.length) this  else target
+
+// this는 getLongString을 호출한 source를 나타낸다!
--- kittybelle/kotlin-part1/src/chap04/section3/InlineFunction.kt
+++ kittybelle/kotlin-part1/src/chap04/section3/InlineFunction.kt
@@ -8,15 +8,27 @@ package chap04.section3
 inline fun shortFunc(a: Int, out: (Int) -> Unit) {
     println("Hi")
     out(a)
+    println("Bye")
 }
+
+// noinline으로 자바 디컴파일 하면 out.invoke가 그대로 남아있다. 펼쳐지지 않고!
+//inline fun shortFunc(a: Int, noinline out: (Int) -> Unit) {
+//    println("Hi")
+//    out(a)
+//}
 
 fun main() {
     // shortFunc() // F7로 함수 프레임 확인 가능
     // inline으로 호출 시 별도의 프레임 생성되지 않음!
 
     shortFunc(3) {a ->  println("a: $a") }
+
     // 인자가 1개면 it으로 쓸 수 있음. {println("a: $it")}
+    // return -> out과 함께 shortFunc라는 함수 자체도 빠져나가, 즉 비지역반환되어 Bye가 출력되지 않는다.
     shortFunc(3) {
         println("a: $it")
+        return
     }
+
+    // 비지역반환을 금지 -> out 앞 crossinline
 }

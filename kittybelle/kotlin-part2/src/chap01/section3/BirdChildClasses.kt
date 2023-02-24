a4127bfa8fe68c4740689e9f22ff7f6fdde363d0
HannaSeo <shn2281@naver.com>
2023-02-25 01:41:02 +0900

    [7주차] 상속과 다형성(2)

--- kittybelle/kotlin-part2/src/chap01/section3/BirdChildClasses.kt
+++ kittybelle/kotlin-part2/src/chap01/section3/BirdChildClasses.kt
@@ -1,12 +1,16 @@
 package chap01.section3
 
 open class Bird(var name: String, var wing: Int, var beak: String) {
-    fun fly() {
+    open fun fly() { // 오버라이딩 가능하게 하려면 open 필요
         println("Fly")
     }
 }
 
 class Lark(name: String, wing: Int, beak: String): Bird(name, wing, beak) {
+    override fun fly() {
+        println("Quick Fly")
+    }
+
     fun singHitone() {
         println("sing Hitone")
     }
@@ -25,12 +29,18 @@ class Parrot: Bird {
     fun speak() {
         println("Speak: $language")
     }
+
+    override fun fly() {
+        println("Slow Fly")
+    }
 }
 
 fun main() {
     val lark = Lark("mylark", 2, "short")
     val parrot = Parrot("myparrot", 2, "long", "English")
 
+    // Bird 타입으로 객체 생성 시 singHitone 사용 불가
+    
     println("lark - name: ${lark.name}")
     println("parrot - name: ${parrot.name}, lang: ${parrot.language}")
 
